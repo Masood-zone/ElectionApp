178b9891cfdd79b5ea5ed9e9005061ce45de138e
Masood Acheampong <masoodacheampong@gmail.com>
2023-06-18 15:28:14 +0100

    Updated positions section.

--- election-app/src/components/button/index.js
+++ election-app/src/components/button/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 const Button = (props) => {
+  const btnStyle = {
+    marginTop: "15px",
+  };
   return (
     <button
       type={props.type}
       className="btn btn-primary"
       onClick={props.onClick}
+      style={btnStyle}
     >
       {props.title}
     </button>
--- election-app/src/pages/candidates/add.js
+++ election-app/src/pages/candidates/add.js
@@ -1,21 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../../components/input";
 import Card from "../../components/card";
+import Button from "../../components/button";
 
 const AddCandidates = () => {
+  const [state, setState] = useState({
+    candidateName: "",
+    telephone: "",
+    profile: "",
+    positions: "",
+  });
+
+  const handleClick = () => {
+    console.log(state);
+  };
   return (
     <div>
-      <form>
+      <form
+        onSubmit={() => {
+          handleClick();
+        }}
+      >
         <Card title="Add Candidates">
           <Input
             name="candidateName"
             type="text"
-            id="candidateId"
+            id="candidateName"
             title="Candidate Name"
+            onChange={(e) => {
+              setState((prevState) => {
+                return { ...prevState, candidateName: e.target.value };
+              });
+            }}
           />
-          <Input name="telephone" id="telephone" type="tel" title="Telephone" />
-          <Input name="profile" id="profile" type="file" title="Profile" />
-          <Input name="Postion" id="positionId" type="text" title="Position" />
+          <Input
+            name="telephone"
+            id="telephone"
+            type="tel"
+            title="Telephone"
+            onChange={(e) => {
+              setState((prevState) => {
+                return { ...prevState, telephone: e.target.value };
+              });
+            }}
+          />
+          <Input
+            name="profile"
+            id="profile"
+            type="file"
+            title="Profile"
+            onChange={(e) => {
+              setState((prevState) => {
+                return { ...prevState, profile: e.target.value };
+              });
+            }}
+          />
+          <Input
+            name="Postion"
+            id="positionId"
+            type="text"
+            title="Position"
+            onChange={(e) => {
+              setState((prevState) => {
+                return { ...prevState, positions: e.target.value };
+              });
+            }}
+          />
+
+          <Button type="submit" title="Submit" />
         </Card>
       </form>
     </div>
