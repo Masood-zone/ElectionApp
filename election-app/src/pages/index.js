a5c31608cf217300523920a7f14a715283fe83b2
Masood Acheampong <masoodacheampong@gmail.com>
2023-06-18 18:24:07 +0100

    Done with positions and modified pages index

--- election-app/src/pages/index.js
+++ election-app/src/pages/index.js
@@ -7,6 +7,9 @@ import VoterList from "./voters/list";
 import Candidates from "./candidates";
 import AddCandidates from "./candidates/add";
 import CandidateList from "./candidates/list";
+import Positions from "./positions";
+import AddPositions from "./positions/add";
+import PositionsList from "./positions/list";
 
 const MainApp = () => {
   return (
@@ -23,6 +26,11 @@ const MainApp = () => {
         <Route path="candidates" element={<Candidates />}>
           <Route path="add" element={<AddCandidates />} />
           <Route path="list" element={<CandidateList />} />
+        </Route>
+
+        <Route path="position" element={<Positions />}>
+          <Route path="add" element={<AddPositions />} />
+          <Route path="list" element={<PositionsList />} />
         </Route>
       </Routes>
     </div>
